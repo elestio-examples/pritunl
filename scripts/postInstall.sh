751fbe406568e68e8fd0532757a15575971ba9aa
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-05 12:53:23 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,11 +4,4 @@ echo "Waiting for software to be ready..."
 sleep 30s;
 
 credentials=$(docker-compose exec -T pritunl bash -c "pritunl default-password")
-password=$(echo "$credentials" | grep -oP 'Password: \K.*')
-
-password=$(echo "$password" | awk '{$1=$1};1')
-
-cat << EOT >> ./.env
-
-ADMIN_PASSWORD=${password}
-EOT
+echo $credentials > default-credentials.txt
